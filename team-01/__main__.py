772b23b2982196865e3270f9deaa1113ec514be5
Eunice Hong <eunjin.hong@pbagel.com>
2023-06-15 10:57:42 +0900

    feat: add prompts and ui elements for step 3 and 4

--- team-01/__main__.py
+++ team-01/__main__.py
@@ -8,6 +8,8 @@ import pandas as pd
 import multiprocessing as mp
 import prompt_step_1
 import prompt_step_2
+import prompt_step_3
+import prompt_step_4
 
 # Load Environment variables and have them as constants
 dotenv.load_dotenv()
@@ -96,11 +98,71 @@ def show_actor_appearance(current_profile):
         print(e)
         return {}
 
+@st.cache_data
+def show_actor_image(current_appearance):
+    '''
+    Step 3. Generate an image of the character.
+    '''
+    st.subheader('이미지 그려보기')
+    messages = prompt_step_3.get_messages(json.dumps(current_appearance))
+    response = send_chat(messages)
+    try:
+        print("Step 3 response")
+        print(response)
+        image_response = openai.Image.create(
+            prompt=prompt_step_3.get_prompt(response.__str__()),
+            n=1,
+            size=f'{512}x{512}',
+        )
+        current_image_url = image_response['data'][0]['url']
+        st.image(current_image_url)
+        return current_image_url
+
+    except Exception as e:
+        print(e)
+        return {}
+
+
+@st.cache_data
+def show_behind_story(current_profile):
+    '''
+    Step 4. Generate a behind story of the character.
+    '''
+    st.subheader('캐릭터 생애 스토리 보기')
+    messages = prompt_step_4.get_messages(json.dumps(current_profile))
+    response = send_chat(messages)
+    try:
+        print("Step 4 response")
+        print(response)
+        actor_detail_json = json.loads(response)
+        current_history = {
+            "precious_memory":actor_detail_json["precious_memory"],
+            "tragic_moment":actor_detail_json["tragic_moment"],
+            "current_life":actor_detail_json["current_life"],
+        }
+
+        st.subheader("소중한 기억")
+        st.write(actor_detail_json["precious_memory"])
+
+        st.subheader("트라우마")
+        st.write(actor_detail_json["tragic_moment"])
+
+        st.subheader("현재")
+        st.write(actor_detail_json["current_life"])
+
+        return current_history
+    except Exception as e:
+        print(e)
+        return {}
+
+
 def set_up_default_view():
     st.title('가상 캐스팅')
     user_input = st.text_input('캐릭터의 성격을 알려주세요.', "유튜버 겸 모델 출신 인플루언서 SNS없이 못 사는 인생은 폼생폼사 20대 초반 남자 캐릭터 만들어 줘.")
     current_profile = show_actor_profile(user_input)
     current_appearance = show_actor_appearance(current_profile)
+    show_actor_image(current_appearance)
+    show_behind_story(current_profile)
 
 
 if __name__ == '__main__':
---
+++ team-01/prompt_step_3.py
@@ -0,0 +1,88 @@
+# Step 2: Generate details about the character using the bio.
+
+import prompt_step_1
+import prompt_step_2
+
+
+SYSTEM_PROMPT = """
+You are a bot that extracts keywords related to the description of appearance within the text.
+A description of a character's appearance and style would be provided.
+
+Follow the steps below to complete the task.
+
+Step 1. Translate the text into English.
+
+The description format you will write is as follows.
+
+Input format(Text):
+"175cm의 중간 키에 근육질인 체격. 각선미가 돋보이는 얼굴에 눈매가 뚜렷하다. 눈동자가 크고 밝아서 상대방의 시선을 잡기 쉽다. 깔끔한 외모 덕분에 오프라인에서도 많은 팬을 모은다. 무신사 스타일을 선호하며 스포츠 브랜드의 의류를 자주 입는다. 또한 캐주얼한 데님 팬츠를 선호하고, 모자나 안경 등 악세사리를 착용하는 것을 좋아한다."
+
+Output format(Text):
+"He is 175cm tall and muscular. The eyes are clear on the face with a sharp angular beauty. His large and bright eyes make it easy to catch the other person's eyes. Thanks to its neat appearance, it also attracts many fans offline. He prefers Musinsa style and often wears sports brand clothes. Also, I prefer casual denim pants, and I like to wear accessories such as hats and glasses."
+
+Step 2. Extract keywords related to the description of appearance within the text.
+
+Input format(Text):
+"He is 175cm tall and muscular. The eyes are clear on the face with a sharp angular beauty. His large and bright eyes make it easy to catch the other person's eyes. Thanks to its neat appearance, it also attracts many fans offline. He prefers Musinsa style and often wears sports brand clothes. Also, I prefer casual denim pants, and I like to wear accessories such as hats and glasses."
+
+Output format(JSON):
+[
+    "175cm tall",
+    "muscular",
+    "clear",
+    "large eyes",
+    "bright eyes",
+    "neat appearance",
+    "fans offline",
+    "sports brand clothes",
+    "casual denim pants",
+    "accessories"
+]
+"""
+
+INPUT_PROMPT_EXAMPLE = prompt_step_2.OUTPUT_PROMPT_EXAMPLE
+
+OUTPUT_PROMPT_EXAMPLE = """
+[
+    "175cm tall",
+    "muscular",
+    "clear",
+    "sharp angular beauty",
+    "large eyes",
+    "bright eyes",
+    "neat appearance",
+    "fans offline",
+    "Musinsa style",
+    "sports brand clothes",
+    "casual denim pants",
+    "accessories",
+    "hats", 
+    "glasses"
+]
+"""
+
+
+
+def get_messages(user_input: str):
+    return [
+        {
+            "role": "system",
+            "content": SYSTEM_PROMPT
+        },
+        {
+            "role": "user",
+            "content": INPUT_PROMPT_EXAMPLE
+        },
+        {
+            "role": "assistant",
+            "content": OUTPUT_PROMPT_EXAMPLE
+        },
+        {
+            "role": "user",
+            "content": user_input
+        }
+    ]
+
+
+def get_prompt(keyword_list):
+    return "Upper body image: Asian man in their 20s celebrity-like with following keywords:" + keyword_list
