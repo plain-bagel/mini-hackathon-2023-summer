d828a3f46573e5c7c8541a8da08a0a15c3526287
Eunice Hong <eunjin.hong@pbagel.com>
2023-06-15 10:56:06 +0900

    refactor: apply prompt to improve the results from step 2

--- team-01/__main__.py
+++ team-01/__main__.py
@@ -69,49 +69,39 @@ def show_actor_profile(user_input):
         return
 
 
-def show_actor_details(actor_bio_data):
+@st.cache_data
+def show_actor_appearance(current_profile):
     '''
     Step 2: Generate a detailed profile of the character.
     '''
-    st.subheader('캐릭터 상세 정보 보기')
-    if not st.button('정보 생성'):
-        st.write('정보 생성을 눌러주세요.')
-    else:
-        actor_bio = {
-            "name": actor_bio_data["이름"].iloc[0], 
-            "age": actor_bio_data["나이"].iloc[0], 
-            "height": actor_bio_data["키"].iloc[0], 
-            "hobby": actor_bio_data["취미"].iloc[0], 
-            "mbti": actor_bio_data["MBTI"].iloc[0], 
-            "personality": actor_bio_data["성격"].iloc[0], 
-            "occupation": actor_bio_data["직업"].iloc[0]
+    st.subheader('캐릭터 외형 정보 보기')
+    messages = prompt_step_2.get_messages(json.dumps(current_profile))
+    response = send_chat(messages)
+    response = send_chat(messages)
+    try:
+        print("Step 2 response")
+        print(response)
+        actor_detail_json = json.loads(response)
+        print(actor_detail_json)
+        current_appearance = {
+            "appearance": actor_detail_json["appearance"],
+            "fashion": actor_detail_json["fashion"],
         }
-        print(actor_bio)
-        messages = prompt_step_2.get_messages(json.dumps(actor_bio))
-        response = send_chat(messages)
-        try:
-            print(response)
-            actor_detail_json = json.loads(response)
-            print(actor_detail_json)
-            profile = {
-                "appearance":actor_detail_json["appearance"],
-                "fashion":actor_detail_json["fashion"],
-            }
-            df = pd.DataFrame([
-                profile,
-            ])
-            return st.table(df)
-        except Exception as e:
-            print(e)
-            return
-        
-
+        st.subheader("외모")
+        st.write(actor_detail_json["appearance"])
+        st.subheader("스타일")
+        st.write(actor_detail_json["fashion"])
+        return current_appearance
+    except Exception as e:
+        print(e)
+        return {}
 
 def set_up_default_view():
     st.title('가상 캐스팅')
     user_input = st.text_input('캐릭터의 성격을 알려주세요.', "유튜버 겸 모델 출신 인플루언서 SNS없이 못 사는 인생은 폼생폼사 20대 초반 남자 캐릭터 만들어 줘.")
-    actor_bio_data = show_actor_profile(user_input)
-    result = show_actor_details(actor_bio_data)
+    current_profile = show_actor_profile(user_input)
+    current_appearance = show_actor_appearance(current_profile)
+
 
 if __name__ == '__main__':
     set_up_default_view()
